462609447da9c95442d78b72b1f5bc663ae93fcc
Maximilien Raulic <raulic.maximilien@gmail.com>
2019-09-12 08:58:52 +0200

    chore(deps-dev): bump pytest from 5.1.1 to 5.1.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 _TEST_REQUIRE = [
-    "pytest==5.1.1",
+    "pytest==5.1.2",
     "pytest-cov==2.7.1",
     "pytest-asyncio==0.10.0",
     "pytest-aiohttp==0.3.0",
