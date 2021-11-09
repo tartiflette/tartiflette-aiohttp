8d056cff1e10f73a7c8d6d1730dd2698f3e49c94
David Baumgold <david@davidbaumgold.com>
2021-11-09 12:26:31 +0100

    support aiohttp 3.8.0

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     keywords="api graphql protocol api rest relay tartiflette dailymotion",
     packages=_PACKAGES,
     install_requires=[
-        "aiohttp>=3.5.4,<3.8.0",
+        "aiohttp>=3.5.4,<3.9.0",
         "async_generator;python_version=='3.6.*'",
         "tartiflette>=0.12.0,<2.0.0",
     ],
